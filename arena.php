14e7a4b76c35bd2ae3bcd8c5834010982dcc5f2a
Ethan Jimenez <erosethanjv@gmail.com>
2013-11-20 18:22:52 -0600

    Create arena.php

---
+++ arena.php
@@ -0,0 +1,93 @@
+<?php
+	include 'config.php';
+
+	session_start();
+	if(!isset($_SESSION['UserId']))
+		// User not logged in.
+		header('Location: login.php');
+	else
+	{
+		if(!isset($_GET['round']))
+			// Round does not exists.
+			header('Location: index.php');
+		else
+		{
+			$RoundId = $_GET['round'];
+			$UserId = $_SESSION['UserId'];
+		
+			mysql_connect(DB_HOST, DB_USER, DB_PASS);
+			mysql_select_db(DB_NAME);
+			
+			$query = sprintf('select RoundName, CodingStart, BurningStart, RoundEnd, RedUserId, Red.UserName as RedName, Blue.UserName as BlueName from `Match` natural join `Round` join User as Red join User as Blue on Red.UserId = RedUserId and Blue.UserId = BlueUserId where RoundId = %d and (RedUserId = %d or BlueUserId = %d)', $RoundId, $UserId, $UserId);
+			$result = mysql_query($query);
+			
+			if(mysql_num_rows($result) == 0)
+				// User not matched in this round.
+				header('Location: index.php');
+			else
+			{
+				$row = mysql_fetch_array($result);
+				mysql_free_result($result);
+				
+				// Get both users name.
+				$UserName = $row['RedName'];
+				$RivalName = $row['BlueName'];
+				if($row['RedUserId'] != $UserId)
+					list($UserName, $RivalName) = array($RivalName, $UserName);
+				
+				// Get round information.
+				$RoundName = $row['RoundName'];	
+				$CodingStart = strtotime($row['CodingStart']);
+				$BurningStart = strtotime($row['BurningStart']);
+				$RoundEnd = strtotime($row['RoundEnd']);
+				
+				// Get actual stage information.
+				if(time() < $CodingStart)
+				{
+					// Round has not started yet.
+					$StageName = 'Coming Soon';
+					$StageMessage = 'Round starts in';
+					$StageRemaining = $CodingStart - time();
+				}
+				elseif(time() < $BurningStart)
+				{
+					// Round is at coding stage.
+					$StageName = 'Coding Stage';
+					$StageMessage = 'Coding ends in';
+					$StageRemaining = $BurningStart - time();
+				}
+				elseif(time() < $RoundEnd)
+				{
+					// Round is at burning stage.
+					$StageName = 'Burning Stage';
+					$StageMessage = 'Round ends in';
+					$StageRemaining = $RoundEnd - time();
+				}
+				else
+				{
+					// Round has ended.
+					$StageName = 'Closure Stage';
+					$StageMessage = 'Round has ended';
+					$StageRemaining = 0;
+				}
+			}
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8"/>
+		<title>CodeBurst! Arena</title>
+	</head>
+	<body>
+		<script src = "jquery.js"></script>
+		<div id = "roundname"><?php echo $RoundName; ?></div>
+		<div id = "stagename"><?php echo $StageName; ?></div>
+		<div id = "stagemessage"><?php echo $StageMessage; ?></div>
+		<div id = "stageremaining"><?php echo $StageRemaining; ?></div>
+		<div id = "username"><?php echo $UserName; ?></div>
+		<div id = "rivalname"><?php echo $RivalName; ?></div>
+	</body>
+</html>
