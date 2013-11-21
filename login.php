de26b13f25d6ac0b6e168a58e737eed64025d3e3
Ethan Jimenez <erosethanjv@gmail.com>
2013-11-20 18:24:46 -0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+	include 'config.php';
+
+	session_start();
+	$_SESSION['UserId'] = 2;
+	/*if(isset($_SESSION['UserId']))
+		header('Location: index.php');
+	else
+	{
+		
+	}*/
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8"/>
+		<title>CodeBurst! Login</title>
+	</head>
+	<body>
+		<script src = "jquery.js"></script>
+	</body>
+</html>
