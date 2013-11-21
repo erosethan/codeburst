d0395670befb75ae52f81ceb9bf1c2352a56b1d9
Ethan Jimenez <erosethanjv@gmail.com>
2013-11-20 22:48:55 -0600

    Update util.php
    All stages name and message has been defined as constants.

--- util.php
+++ util.php
@@ -8,34 +8,39 @@
 		define('BURN_STAGE', 'Burning');
 		define('CLOS_STAGE', 'Closure');
 		
+		define('WAIT_MSG', 'Ronda comienza en');
+		define('CODE_MSG', 'Coding termina en');
+		define('BURN_MSG', 'Ronda termina en');
+		define('CLOS_MSG', 'Ronda finalizada');
+		
 		function StageInfo($CodingStart, $BurningStart, $RoundEnd){
 			$ActualTime = time();
 			if($ActualTime < $CodingStart)
 			{
 				// Round has not started yet.
 				$StageName = WAIT_STAGE;
-				$StageMessage = 'Ronda empieza en';
+				$StageMessage = WAIT_MSG;
 				$StageRemaining = $CodingStart - $ActualTime;
 			}
 			elseif($ActualTime < $BurningStart)
 			{
 				// Round is at coding stage.
 				$StageName = CODE_STAGE;
-				$StageMessage = 'Coding termina en';
+				$StageMessage = CODE_MSG;
 				$StageRemaining = $BurningStart - $ActualTime;
 			}
 			elseif($ActualTime < $RoundEnd)
 			{
 				// Round is at burning stage.
 				$StageName = BURN_STAGE;
-				$StageMessage = 'Ronda termina en';
+				$StageMessage = BURN_MSG;
 				$StageRemaining = $RoundEnd - $ActualTime;
 			}
 			else
 			{
 				// Round has ended.
 				$StageName = CLOS_STAGE;
-				$StageMessage = 'Ronda finalizada';
+				$StageMessage = CLOS_MSG;
 				$StageRemaining = 0;
 			}
 			return array($StageName, $StageMessage, $StageRemaining);
