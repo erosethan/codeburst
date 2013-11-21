c380df19e77528f094f16aee362e8fb0c10bc067
Adonais Romero Gonzalez <adonais.neoadonis@gmail.com>
2013-11-21 17:50:05 -0600

    Updated submit.php and forms.php to accept different types of uploads and save them in different folders according to round and user id. Still needed to receive round data such as id and if still running.

--- forms.php
+++ forms.php
@@ -2,54 +2,58 @@
 	if(!defined('FORMS_INCLUDE'))
 	{
 		define('FORMS_INCLUDE', true);
-	
+
 		function FileSubmit($FileType)
 		{
+?>
+		<form action="submit.php" method="post" enctype="multipart/form-data">
+<?php
 			if($FileType == 'code')
 			{
-			?>
-				<form action = "submit.php?type=code" method = "post" enctype = "multipart/form-data">
-					<p>
-						<label for = "lang">Lenguaje:</label>
-						<select name = "lang">
-							<option value = "c">C</option>
-							<option value = "cpp">C++</option>
-							<option value = "java">Java</option>
-							<option value = "py">Python</option>
-						</select>
-					</p>
-					<p>
-						<label for = "file">Súbelo desde un archivo:<br/></label>
-						<input type = "file" name = "file" id = "file"/>
-					</p>
-					<p>
-						<label for = "code">Súbelo copiando el código:<br/></label>
-						<textarea name = "code" id = "code"></textarea>
-					</p>
-					<p>
-						<input type = "submit" value = "Subir código"/>
-					</p>
-				</form>
-			<?php
+?>
+			<input type="hidden" name="type" value="code"/>
+			<p>
+				<label for="lang">Lenguaje:</label>
+				<select name="lang">
+					<option value = "c">C</option>
+					<option value = "cpp">C++</option>
+					<option value = "java">Java</option>
+					<option value = "py">Python</option>
+				</select>
+			</p>
+			<p>
+				<label for="file">Súbelo desde un archivo:<br/></label>
+				<input type="file" name="file" id="file"/>
+			</p>
+			<p>
+				<label for="dinput">Súbelo copiando el código:<br/></label>
+				<textarea name="dinput" id="dinput"></textarea>
+			</p>
+			<p>
+				<input type="submit" value="Subir código"/>
+			</p>
+<?php
 			}
 			elseif($FileType == 'burn')
 			{
-			?>
-				<form action = "submit.php?type=burn" method = "post" enctype = "multipart/form-data">
-					<p>
-						<label for = "file">Súbelo desde un archivo:<br/></label>
-						<input type = "file" name = "file" id = "file"/>
-					</p>
-					<p>
-						<label for = "burn">Súbelo copiando el burn:<br/></label>
-						<textarea name = "burn" id = "burn"></textarea>
-					</p>
-					<p>
-						<input type = "submit" value = "Subir burn"/>
-					</p>
-				</form>
-			<?php
+?>
+			<input type="hidden" name="type" value="burn"/>
+			<p>
+				<label for="file">Súbelo desde un archivo:<br/></label>
+				<input type="file" name="file" id="file"/>
+			</p>
+			<p>
+				<label for="dinput">Súbelo copiando el burn:<br/></label>
+				<textarea name="dinput" id="dinput"></textarea>
+			</p>
+			<p>
+				<input type="submit" value="Subir burn"/>
+			</p>
+<?php
 			}
+?>
+		</form>
+<?php
 		}
 	}
 ?>
--- submit.php
+++ submit.php
@@ -1,15 +1,42 @@
 <?php
+
 	$VALID_LANG = Array('c', 'cpp', 'java', 'py');
-	$dir = 'codes';
-	$subdir = 'test'; // TODO
-	$filename = 'coso'; // TODO
-	$lang = $_POST['lang'];
-	if (!in_array($lang, $VALID_LANG))
-		die('ERROR'); // TODO
-	$filename .= ".$lang";
-	$route = "$dir/$subdir/$filename";
-	if ($_FILES["file"]["error"] == 0)
-		move_uploaded_file($_FILES["file"]["tmp_name"], $route);
+	$type = $_POST['type'];
+	$roundid = 0; // TODO
+
+	switch ($type) {
+
+		case 'code':
+			$dir = 'codes';
+			$lang = $_POST['lang'];
+			$userid = 0; // TODO
+			$filename = "{$roundid}_{$userid}";
+			if (!in_array($lang, $VALID_LANG))
+				die('ERROR'); // TODO
+			$filename .= ".$lang";
+			break;
+
+		case 'burn':
+			$dir = 'burns';
+			$burnedid = 0; // TODO
+			$filename = "{$roundid}_{$burnedid}.txt";
+			break;
+
+		default:
+			die('ERROR'); // TODO
+
+	}
+
+	$subdir = $roundid;
+	$dirroute = "$dir/$subdir";
+
+	if (!file_exists($dirroute))
+		mkdir($dirroute, 0777, true);
+
+	$route = "$dirroute/$filename";
+
+	if ($_FILES['file']['error'] == 0)
+		move_uploaded_file($_FILES['file']['tmp_name'], $route);
 	else
-		file_put_contents($route, $_POST['code']);
+		file_put_contents($route, $_POST['dinput']);
 ?>
