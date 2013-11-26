74bc03dbeba478fac24e5b6bcec719434633c294
Ethan Jimenez <erosethanjv@gmail.com>
2013-11-25 22:24:17 -0600

    Update header.php encoding

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <div id="nav">
 	<ul class="box">
 		<li id="nav-active"><a href="index.php">Inicio</a></li>
-		<li><a href="logout.php">Cerrar sesión</a></li>
+		<li><a href="logout.php">Cerrar sesiÃ³n</a></li>
 	</ul>
 	<hr class="noscreen" /> 
 </div>
