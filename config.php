6f8b7c03eaedcd3de8560a7dc140cd645711ad9c
juniocarl <juniocarl@gmail.com>
2013-11-28 11:41:59 -0600

    Fix to assert warning

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
 	// Timezone setting.
 	define('GLOBAL_TIMEZONE', 'America/Mexico_City'); // Refer to http://us2.php.net/manual/en/timezones.php
 	$timezone_correct = date_default_timezone_set(GLOBAL_TIMEZONE);
-	assert($timezone_correct, "Global timezone incorrectly set.");
+	if(!$timezone_correct) die();
 	
 	echo date("r");
 ?>
